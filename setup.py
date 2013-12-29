1e381801e0299db124d2d8fc854f7f9fff99827f
Robin Orheden <robin.orheden@userapp.io>
2013-12-29 15:50:20 +0100

    Added pip support.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='userapp',
+      version=version,
+      description="Client for accessing the UserApp API.",
+      long_description="""\
+Client library for making calls to the UserApp API.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='user management api authentication oauth',
+      author='Robin Orheden',
+      author_email='robin.orheden@userapp.io',
+      url='https://www.userapp.io/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
